8bc662bb424d1abffe28916ef911cf0f438c1e67
Chris Ye <chrisyeshi@gmail.com>
2018-06-14 00:29:37 -0700

    added in network references and citations

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -5,10 +5,11 @@
     <span>{{ paper.authors }}</span>
     <div class="graph-container" ref="graphContainer" v-on:wheel="scrollHorizontally">
       <div class="nodes-container">
-        <paper-card v-for="node in nodes" v-bind:paper="node" v-bind:key="node.key"></paper-card>
+        <paper-card v-for="node in nodes" v-bind:paper="node" v-bind:key="node.key" v-on:linkreferences="linkInNetworkReferences" v-on:linkcitations="linkInNetworkCitations"></paper-card>
       </div>
       <svg class="overlay">
-        <line v-for="link in links" :key="link.key" :x1="link.citedBy.x" :y1="link.citedBy.y" :x2="link.citing.x" :y2="link.citing.y"></line>
+        <!-- <line v-for="link in links" :key="link.key" :x1="link.citedBy.x" :y1="link.citedBy.y" :x2="link.citing.x" :y2="link.citing.y"></line> -->
+        <path v-for="curve in curves" :key="curve.key" :d="curve.path"></path>
       </svg>
     </div>
   </div>
@@ -46,13 +47,17 @@ export default {
 
       let colWidth = 300
       let nodeSpacing = 20
-      let nodeHeight = 100
+      let nodeHeight = 94
+      let headerHeight = 20
       let getNodeRect = (paper) => {
         return {
           left: (colWidth + nodeSpacing) * yearUniqueStr.indexOf(paper.year.toString()),
           top: (nodeHeight + nodeSpacing) * yearPapers[paper.year.toString()].indexOf(paper),
           width: colWidth,
           height: nodeHeight,
+          get right () {
+            return this.left + this.width
+          },
           get center () {
             return {
               x: this.left + this.width / 2,
@@ -69,7 +74,10 @@ export default {
           authors: paper.authors,
           year: paper.year,
           citationCount: paper.citationCount,
+          inNetworkReferenceCount: this.getInNetworkReferenceCount(index),
+          inNetworkCitationCount: this.getInNetworkCitationCount(index),
           rect: nodeRect,
+          headerHeight: headerHeight,
           get style () {
             return {
               left: nodeRect.left + 'px',
@@ -86,26 +94,47 @@ export default {
         relations: []
       },
       paper: {},
-      nodes: []
+      nodes: [],
+      visibleRelations: []
     }
   },
   computed: {
     links: function () {
-      return this.data.relations.map((relation, index) => {
+      return this.visibleRelations.map((relation, index) => {
         let citedBy = this.nodes[relation.citedBy]
         let citing = this.nodes[relation.citing]
-        let citedByCtr = citedBy.rect.center
-        let citingCtr = citing.rect.center
         return {
           key: index,
           citedBy: {
-            x: citedByCtr.x,
-            y: citedByCtr.y
+            x: citedBy.rect.left,
+            y: citedBy.rect.top + citedBy.headerHeight / 2
           },
           citing: {
-            x: citingCtr.x,
-            y: citingCtr.y
+            x: citing.rect.right,
+            y: citing.rect.top + citing.headerHeight / 2
           }
+        }
+      })
+    },
+    curves: function () {
+      return this.visibleRelations.map((relation, index) => {
+        let citedBy = this.nodes[relation.citedBy]
+        let citing = this.nodes[relation.citing]
+        const start = {
+          x: citedBy.rect.left,
+          y: citedBy.rect.top + citedBy.headerHeight / 2
+        }
+        const end = {
+          x: citing.rect.right,
+          y: citing.rect.top + citing.headerHeight / 2
+        }
+        const interpolate = (beg, end, ratio) => {
+          return ratio * (end - beg) + beg
+        }
+        const ratio = 0.75
+        return {
+          key: index,
+          path: `M${start.x} ${start.y} C ${interpolate(start.x, end.x, ratio)} ${start.y}, ${interpolate(end.x, start.x, ratio)} ${end.y}, ${end.x} ${end.y}`
         }
       })
     }
@@ -115,6 +144,31 @@ export default {
       evt.preventDefault()
       let delta = evt.deltaX === 0 ? -evt.deltaY : -Math.sign(evt.deltaX) * Math.hypot(evt.deltaX, evt.deltaY)
       this.$refs.graphContainer.scrollLeft -= delta * 5
+    },
+    getRelations: function (prop, paperIndex) {
+      const IsPaperInRelation = relation => relation[prop] === paperIndex
+      return this.data.relations.filter(IsPaperInRelation)
+    },
+    // getInNetworkReferences: function (paperIndex) {
+    //   const getCitingPaperIndex = relation => relation.citing
+    //   const getPaper = paperIndex => this.nodes[paperIndex]
+    //   const relations = this.getRelations('citedBy', paperIndex)
+    //   return relations.map(getCitingPaperId).map(getPaper)
+    // },
+    getInNetworkReferenceCount: function (paperIndex) {
+      return this.getRelations('citedBy', paperIndex).length
+    },
+    getInNetworkCitationCount: function (paperIndex) {
+      return this.getRelations('citing', paperIndex).length
+    },
+    linkInNetworkReferences: function (paperIndex) {
+      this.showLinks(this.getRelations('citedBy', paperIndex))
+    },
+    linkInNetworkCitations: function (paperIndex) {
+      this.showLinks(this.getRelations('citing', paperIndex))
+    },
+    showLinks: function (relations) {
+      this.visibleRelations = relations
     }
   }
 }
@@ -145,6 +199,13 @@ export default {
   stroke: #55a;
   fill: none;
   stroke-width: 3px;
+}
+
+.overlay path {
+  stroke: #a55;
+  fill: none;
+  stroke-opacity: 0.7;
+  stroke-width: 20px;
 }
 
 .kanban {
--- src/components/PaperCard.vue
+++ src/components/PaperCard.vue
@@ -1,34 +1,117 @@
 <template>
-  <div class="paper-card" v-bind:index="paper.index" v-bind:style="paper.style" v-on:mousedown.prevent="dragElement">
-    <div>{{ paper.title }}</div>
-    <span>{{ paper.authors }}</span>
-    <span>Cited by {{ paper.citationCount }}</span>
+  <div class="paper-card" v-bind:index="paper.index" v-bind:style="paper.style">
+    <div class="header" v-bind:style="{ height: paper.headerHeight + 'px' }">
+      <span class="header-references" v-on:mouseover="$emit('linkreferences', paper.key)" v-bind:style="{ 'background-color': inNetworkReferenceColor }">&lt; {{ paper.inNetworkReferenceCount }}</span>
+      <span class="header-bar" v-on:mousedown="dragElement"></span>
+      <span class="header-citations" v-on:mouseover="$emit('linkcitations', paper.key)" v-bind:style="{ 'background-color': inNetworkCitationColor }">{{ paper.inNetworkCitationCount }} &gt;</span>
+    </div>
+    <div v-bind:class="line.classes" v-for="(line, index) in lines" v-bind:key="index">{{ line.text }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'PaperCard',
-  props: ['paper'],
+  props: {
+    paper: Object,
+    draggable: {
+      type: Boolean,
+      default: true
+    },
+    lineCount: {
+      type: Number,
+      default: 4
+    }
+  },
+  computed: {
+    lines: function () {
+      let createLineObj = (classes, text) => {
+        return {
+          classes: classes,
+          text: text
+        }
+      }
+      let authorList = this.paper.authors.split(/, |, and | and /)
+      let shortAuthors = authorList.length < 2 ? authorList[0] : authorList[0] + ' +' + (authorList.length - 1)
+      if (this.lineCount < 2) {
+        return [
+          createLineObj('line-clamp-1', shortAuthors + ', ' + this.paper.year + ', "' + this.paper.citationCount)
+        ]
+      }
+      if (this.lineCount < 3) {
+        return [
+          createLineObj('line-clamp-1', this.paper.title),
+          createLineObj('line-clamp-1', shortAuthors + ', ' + this.paper.year + ', "' + this.paper.citationCount)
+        ]
+      }
+      if (this.lineCount < 4) {
+        return [
+          createLineObj('line-clamp-1', this.paper.title),
+          createLineObj('line-clamp-1', this.paper.authors),
+          createLineObj('line-clamp-1', 'Published in ' + this.paper.year + ', Cited by ' + this.paper.citationCount)
+        ]
+      }
+      if (this.lineCount < 5) {
+        return [
+          createLineObj('line-clamp-2', this.paper.title),
+          createLineObj('line-clamp-1', this.paper.authors),
+          createLineObj('line-clamp-1', 'Published in ' + this.paper.year + ', Cited by ' + this.paper.citationCount)
+        ]
+      }
+      return [
+        createLineObj('', this.paper.title),
+        createLineObj('', this.paper.authors),
+        createLineObj('line-clamp-1', 'Published in ' + this.paper.year + ', Cited by ' + this.paper.citationCount)
+      ]
+    },
+    inNetworkCitationColor: function () {
+      return this.getColor({
+        maxLevel: 4,
+        maxValue: 200,
+        logBase: 2,
+        value: this.paper.inNetworkCitationCount
+      })
+    },
+    inNetworkReferenceColor: function () {
+      return this.getColor({
+        maxLevel: 4,
+        maxValue: 200,
+        logBase: 2,
+        value: this.paper.inNetworkReferenceCount
+      })
+    }
+  },
   methods: {
     dragElement: function (evt) {
+      if (!this.draggable) {
+        return
+      }
+      evt.preventDefault()
       let xPrev = evt.clientX
       let yPrev = evt.clientY
       let closeDragElement = evt => {
-        document.onmosueup = null
+        document.onmouseup = null
         document.onmousemove = null
       }
       let elementDrag = evt => {
         let node = this.paper
         let xCurr = evt.clientX
         let yCurr = evt.clientY
-        node.rect.left += xCurr - xPrev
-        node.rect.top += yCurr - yPrev
+        node.rect.left += (xCurr - xPrev)
+        node.rect.top += (yCurr - yPrev)
         xPrev = xCurr
         yPrev = yCurr
       }
       document.onmouseup = closeDragElement
       document.onmousemove = elementDrag
+    },
+    getColor: function ({ maxLevel, maxValue, logBase, value }) {
+      const log = x => Math.log(x) / Math.log(logBase)
+      const count = Math.floor(value)
+      const unboundLevel = count === 0 ? 0 : Math.floor(log(count)) + 1
+      const boundLevel = Math.min(maxLevel, unboundLevel)
+      const colorValue = maxValue / maxLevel * boundLevel
+      return `rgb(10, ${colorValue}, 50)`
     }
   }
 }
@@ -44,8 +127,44 @@ export default {
   overflow-y: hidden;
 }
 
-.paper-card div,
-.paper-card span {
-  pointer-events: none;
+.line-clamp-1 {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.line-clamp-2 {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.header-references,
+.header-citations {
+  color: white;
+  height: 100%;
+}
+
+.header-references {
+  order: -1;
+}
+
+.header-citations {
+  order: 1;
+}
+
+.header-bar {
+  order: 0;
+  flex-grow: 1;
+  height: 100%;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: gray;
 }
 </style>
