13186d67e4e6590600d3d949e39e31404991a5e7
Chris Ye <chrisyeshi@gmail.com>
2018-06-19 16:48:20 -0700

    add layout by reference levels

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -25,69 +25,10 @@ export default {
   },
   data () {
     this.$http.get('/static/insitupdf.json').then(function (res) {
-      let data = res.body
-      this.data = data
-      let years = data.references.map(paper => paper.year)
-      let onlyUnique = (value, index, self) => self.indexOf(value) === index
-      let yearUniqueNum = years.filter(onlyUnique).sort((a, b) => a - b)
-      let yearUniqueStr = yearUniqueNum.map(year => year.toString())
-      this.columns = yearUniqueStr
-      let yearPapers = {}
-      yearUniqueStr.forEach(yearStr => {
-        yearPapers[yearStr] = []
-      })
-      data.references.forEach(paper => {
-        yearPapers[paper.year.toString()].push(paper)
-      })
-      yearUniqueStr.forEach(yearStr => {
-        yearPapers[yearStr].sort((a, b) => b.citationCount - a.citationCount)
-      })
-      this.colItems = yearPapers
-      this.paper = data.paper
-
-      let colWidth = 300
-      let nodeSpacing = 20
-      let nodeHeight = 94
-      let headerHeight = 20
-      let getNodeRect = (paper) => {
-        return {
-          left: (colWidth + nodeSpacing) * yearUniqueStr.indexOf(paper.year.toString()),
-          top: (nodeHeight + nodeSpacing) * yearPapers[paper.year.toString()].indexOf(paper),
-          width: colWidth,
-          height: nodeHeight,
-          get right () {
-            return this.left + this.width
-          },
-          get center () {
-            return {
-              x: this.left + this.width / 2,
-              y: this.top + this.height / 2
-            }
-          }
-        }
-      }
-      this.nodes = data.references.map((paper, index) => {
-        let nodeRect = getNodeRect(paper)
-        return {
-          key: index,
-          title: paper.title,
-          authors: paper.authors,
-          year: paper.year,
-          citationCount: paper.citationCount,
-          inNetworkReferenceCount: this.getInNetworkReferenceCount(index),
-          inNetworkCitationCount: this.getInNetworkCitationCount(index),
-          rect: nodeRect,
-          headerHeight: headerHeight,
-          get style () {
-            return {
-              left: nodeRect.left + 'px',
-              top: nodeRect.top + 'px',
-              width: nodeRect.width + 'px',
-              height: nodeRect.height + 'px'
-            }
-          }
-        }
-      })
+      this.data = res.body
+      this.paper = this.data.paper
+      // this.nodes = this.layoutByYears(this.data)
+      this.nodes = this.layoutByRefLevel(this.data)
     })
     return {
       data: {
@@ -145,9 +86,21 @@ export default {
       let delta = evt.deltaX === 0 ? -evt.deltaY : -Math.sign(evt.deltaX) * Math.hypot(evt.deltaX, evt.deltaY)
       this.$refs.graphContainer.scrollLeft -= delta * 5
     },
-    getRelations: function (prop, paperIndex) {
+    getRelations: function (relations, prop, paperIndex) {
       const IsPaperInRelation = relation => relation[prop] === paperIndex
-      return this.data.relations.filter(IsPaperInRelation)
+      return relations.filter(IsPaperInRelation)
+    },
+    getReferenceIds: function (relations, paperIndex) {
+      return this.getRelations(relations, 'citedBy', paperIndex).map(relation => relation.citing)
+    },
+    getReferenceCount: function (relations, paperIndex) {
+      return this.getRelations(relations, 'citedBy', paperIndex).length
+    },
+    getCitationCount: function (relations, paperIndex) {
+      return this.getRelations(relations, 'citing', paperIndex).length
+    },
+    getInNetworkRelations: function (prop, paperIndex) {
+      return this.getRelations(this.data.relations, prop, paperIndex)
     },
     // getInNetworkReferences: function (paperIndex) {
     //   const getCitingPaperIndex = relation => relation.citing
@@ -156,19 +109,154 @@ export default {
     //   return relations.map(getCitingPaperId).map(getPaper)
     // },
     getInNetworkReferenceCount: function (paperIndex) {
-      return this.getRelations('citedBy', paperIndex).length
+      return this.getInNetworkRelations('citedBy', paperIndex).length
     },
     getInNetworkCitationCount: function (paperIndex) {
-      return this.getRelations('citing', paperIndex).length
+      return this.getInNetworkRelations('citing', paperIndex).length
     },
     linkInNetworkReferences: function (paperIndex) {
-      this.showLinks(this.getRelations('citedBy', paperIndex))
+      this.showLinks(this.getInNetworkRelations('citedBy', paperIndex))
     },
     linkInNetworkCitations: function (paperIndex) {
-      this.showLinks(this.getRelations('citing', paperIndex))
+      this.showLinks(this.getInNetworkRelations('citing', paperIndex))
     },
     showLinks: function (relations) {
       this.visibleRelations = relations
+    },
+    layoutByYears: function (data) {
+      let years = data.references.map(paper => paper.year)
+      let onlyUnique = (value, index, self) => self.indexOf(value) === index
+      let yearUniqueNum = years.filter(onlyUnique).sort((a, b) => a - b)
+      let yearUniqueStr = yearUniqueNum.map(year => year.toString())
+      let yearPapers = {}
+      yearUniqueStr.forEach(yearStr => {
+        yearPapers[yearStr] = []
+      })
+      data.references.forEach(paper => {
+        yearPapers[paper.year.toString()].push(paper)
+      })
+      yearUniqueStr.forEach(yearStr => {
+        yearPapers[yearStr].sort((a, b) => b.citationCount - a.citationCount)
+      })
+      let getNodeRect = (paper) => {
+        return this.getRectFromColRow({
+          col: yearUniqueStr.indexOf(paper.year.toString()),
+          row: yearPapers[paper.year.toString()].indexOf(paper)
+        })
+      }
+      return data.references.map((paper, index) => {
+        return this.makeNode({
+          paper: paper,
+          key: index,
+          inNetworkReferenceCount: this.getReferenceCount(data.relations, index),
+          inNetworkCitationCount: this.getCitationCount(data.relations, index),
+          rect: getNodeRect(paper)
+        })
+      })
+    },
+    makeRect: function ({ left, top, width, height }) {
+      return {
+        left: left,
+        top: top,
+        width: width,
+        height: height,
+        get right () {
+          return this.left + this.width
+        },
+        get center () {
+          return {
+            x: this.left + this.width / 2,
+            y: this.top + this.height / 2
+          }
+        }
+      }
+    },
+    makeNode: function ({ paper, key, inNetworkReferenceCount, inNetworkCitationCount, rect }) {
+      const headerHeight = 20
+      return {
+        key: key,
+        title: paper.title,
+        authors: paper.authors,
+        year: paper.year,
+        citationCount: paper.citationCount,
+        inNetworkReferenceCount: inNetworkReferenceCount,
+        inNetworkCitationCount: inNetworkCitationCount,
+        rect: rect,
+        headerHeight: headerHeight,
+        get style () {
+          return {
+            left: rect.left + 'px',
+            top: rect.top + 'px',
+            width: rect.width + 'px',
+            height: rect.height + 'px'
+          }
+        }
+      }
+    },
+    layoutByRefLevel: function ({ paper, references, relations }) {
+      const refLevels = this.assignRefLevels(references, relations)
+      const colRows = this.paperColRowsFromRefLevels(refLevels)
+      const paperRects = this.getRectsFromColRows(colRows)
+      return references.map((paper, paperId) => this.makeNode({
+        paper: paper,
+        key: paperId,
+        inNetworkReferenceCount: this.getReferenceCount(relations, paperId),
+        inNetworkCitationCount: this.getCitationCount(relations, paperId),
+        rect: paperRects[paperId]
+      }))
+    },
+    assignRefLevels: function (references, relations) {
+      let refLevels = {}
+      const rootIds = this.getRootIds(references, relations)
+      rootIds.forEach(paperId => { refLevels[paperId] = 0 })
+      let bfsQueue = [].concat(rootIds)
+      while (bfsQueue.length > 0) {
+        const currId = bfsQueue.shift()
+        const refIds = this.getReferenceIds(relations, currId)
+        refIds.forEach(paperId => {
+          refLevels[paperId] = refLevels[paperId] ? Math.max(refLevels[currId] + 1, refLevels[paperId]) : refLevels[currId] + 1
+          bfsQueue.push(paperId)
+        })
+      }
+      return refLevels
+    },
+    getRootIds: function (references, relations) {
+      const paperIds = references.map((paper, index) => index)
+      const rootIds = paperIds.filter(paperId => this.getCitationCount(relations, paperId) === 0)
+      return rootIds
+    },
+    paperColRowsFromRefLevels: function (refLevels) {
+      let colRows = {}
+      let columnCounters = []
+      const maxRefLevel = Math.max(...Object.keys(refLevels).map(index => refLevels[index]))
+      Object.keys(refLevels).forEach(paperId => {
+        const colId = maxRefLevel - refLevels[paperId]
+        const rowId = columnCounters[colId] ? columnCounters[colId] : 0
+        colRows[paperId] = {
+          col: colId,
+          row: rowId
+        }
+        columnCounters[colId] = rowId + 1
+      })
+      return colRows
+    },
+    getRectFromColRow: function ({ col, row }) {
+      let colWidth = 300
+      let nodeSpacing = 20
+      let nodeHeight = 105
+      return this.makeRect({
+        left: (colWidth + nodeSpacing) * col,
+        top: (nodeHeight + nodeSpacing) * row,
+        width: colWidth,
+        height: nodeHeight
+      })
+    },
+    getRectsFromColRows: function (colRows) {
+      let rects = {}
+      Object.keys(colRows).forEach(paperId => {
+        rects[paperId] = this.getRectFromColRow(colRows[paperId])
+      })
+      return rects
     }
   }
 }
