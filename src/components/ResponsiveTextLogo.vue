b1614c927dd602ca8396c9fbdaf77ce50ea77612
Chris Ye <chrisyeshi@gmail.com>
2018-10-04 15:32:36 -0700

    fixed "D" and "isco" in responsive-text-logo to be separated at viewport width 959px

--- src/components/ResponsiveTextLogo.vue
+++ src/components/ResponsiveTextLogo.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-flex d-flex style="cursor: pointer;" v-resize="onResize"
+  <v-layout align-center style="cursor: pointer; min-width: 30px;" v-resize="onResize"
     @click="$emit('click', $event)">
     <span class="grey--text text--darken-2 display-1">D</span>
-    <v-scroll-x-reverse-transition>
-      <span v-if="windowSize.width > 600"
+    <v-slide-x-transition>
+      <span v-if="windowSize.width > 960"
         class="grey--text text--darken-2 display-1">
         isco
       </span>
-    </v-scroll-x-reverse-transition>
-  </v-flex>
+    </v-slide-x-transition>
+  </v-layout>
 </template>
 
 <script>
