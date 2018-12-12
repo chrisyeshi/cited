1db92588df4d956792ee709b843916b3aa5f34ca
Chris Ye <chrisyeshi@gmail.com>
2018-12-11 22:49:05 -0800

    update landing page layout and text

--- client/src/components/LandingPage.vue
+++ client/src/components/LandingPage.vue
@@ -7,93 +7,86 @@
         <v-btn flat href="#sign-up">Sign Up</v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <v-content fluid>
-      <section class="mb-5">
-        <v-container>
-          <h1 class="mt-4 responsive-font-size">
-            Discovery Engine
-          </h1>
-          <h3 class="headline text-xs-center">
-            A paper discovery and management tool for researchers helping researchers.
-          </h3>
-          <div class="text-xs-center" style="margin-bottom: 1em;">
-            <v-btn large color="success" to="/tour/0">Demo</v-btn>
-            <v-btn large color="error" href="#sign-up">Sign Up</v-btn>
-          </div>
-          <v-divider></v-divider>
-          <section style="height: 350px; overflow: hidden; display: flex; justify-content: center;">
-            <img ref="discovery" class="translateX" src="../assets/graph-five.png"
-              style="height: 100%;">
-          </section>
-          <v-layout row wrap align-center>
-            <v-flex xs12 md4>
-              <v-card class="elevation-0 transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline text-xs-center">Researcher-Centric Design</div>
-                </v-card-title>
-                <v-card-text>
-                  Researchers are proposing cutting-edge ideas to transform the human race everyday.
-                  However, current search engines and research assistant tools are not designed around researchers' habits,
-                  in which researchers often need to manually traverse references and citations of interested papers.
-                  Here we aim to provide a highly usable tool for researchers to easily discover the most relevant papers to their research tasks.
-                </v-card-text>
-              </v-card>
+    <v-content>
+      <section class="py-5 px-3 my-5">
+        <v-container grid-list-lg>
+          <v-layout column>
+            <v-flex>
+              <h1 class="mt-5 responsive-font-size">
+                Discovery Engine
+              </h1>
             </v-flex>
-            <v-flex xs12 md4>
-              <v-card class="elevation-0 transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline text-xs-center">Smart Management</div>
-                </v-card-title>
-                <v-card-text>
-                  Our tool can help researchers to effectively collect, organize and management related papers for different research topics.
-                  For each collections of papers created by researchers, our tool automatically arrange and visualize the papers and their relationships in meaningful graphs.
-                  Graph visualization allows people to see the inner relationships and identify the most important or influential papers.
-                </v-card-text>
-              </v-card>
+            <v-flex>
+              <h3 class="title font-weight-light text-xs-center">
+                A new paper discovery experience, for researchers, by researchers.
+              </h3>
             </v-flex>
-            <v-flex xs12 md4>
-              <v-card class="elevation-0 transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Collaborative Discovery</div>
-                </v-card-title>
-                <v-card-text>
-                  We aim to bring the research community together to help junior researchers.
-                  Researchers can share these collections with their peers and also generate references from these collections to help them write papers.
-                  We believe the community can help by providing anonymous reviews and comments.
-                  Together, we can build a strong community and a database with the most accurate meta data of research papers.
-                </v-card-text>
-              </v-card>
+            <v-flex class="mt-4 text-xs-center">
+              <v-btn large color="secondary" to="/tour/0">Demo</v-btn>
+              <v-btn large color="error" href="#sign-up">Sign Up</v-btn>
             </v-flex>
           </v-layout>
         </v-container>
       </section>
-      <v-divider></v-divider>
-      <section>
-        <!-- <v-parallax height=350 class="community"
-          :src="getImgUrl('hans-peter-gauster-1024-unsplash.jpg')">
-        </v-parallax> -->
-        <!-- Photo by Hans-Peter Gauster on Unsplash -->
-      </section>
-      <v-divider></v-divider>
-      <section>
-        <v-parallax height=350 class="collection"
-          :src="getImgUrl('graph-insitu.png')">
-        </v-parallax>
-      </section>
-      <v-divider></v-divider>
-      <section class="mb-5">
+      <section class="py-5 my-5">
         <v-container>
-          <h1 class="mt-2">Why We Build This Tool</h1>
-          <p class="mt-3 subheading">
-            We are so used to looking for new papers based on the references/citations of the papers we found interesting. However, it is surprising that no tool is providing this functionality.
-            After we read a paper, we often have questions, comments, and even new ideas that we want to discuss with other researchers. However, there isn’t a place for this.
-          </p>
+          <v-layout row wrap align-center>
+            <v-flex xs12 md6 style="height: 350px; overflow: hidden; display: flex; justify-content: center;">
+              <img ref="discovery" class="translateX"
+                src="../assets/graph-eight.png" style="height: 100%;">
+            </v-flex>
+            <v-flex xs12 md6 class="py-5" :px-5="$vuetify.breakpoint.smAndUp"
+              :px-4="$vuetify.breakpoint.xsOnly">
+              <h2 class="display-1 font-weight-light">Paper Discovery</h2>
+              <p class="subheading mt-4">
+                We are so used to looking for new papers by tracing from the citations of the papers we found interesting. Discovery engine facilitates this habit with graph visualization, which allows us to not only search papers by content, but also discover papers by traversing the paper relationships.
+              </p>
+            </v-flex>
+          </v-layout>
         </v-container>
       </section>
-      <v-divider></v-divider>
-      <section id="sign-up" class="pb-5 grey darken-3 white--text">
-        <v-container grid-list-md>
-          <h1 class="mt-2 text--white">Sign up for product update:</h1>
+      <section class="py-5 my-5">
+        <v-container>
+          <v-layout row wrap align-center>
+            <v-flex xs12 md6 order-md2>
+              <v-parallax height=350 class="community"
+                :src="getImgUrl('hans-peter-gauster-1024-unsplash.jpg')">
+              </v-parallax>
+            <!-- Photo by Hans-Peter Gauster on Unsplash -->
+            </v-flex>
+            <v-flex xs12 md6 order-md1 class="py-5" :px-5="$vuetify.breakpoint.smAndUp"
+              :px-4="$vuetify.breakpoint.xsOnly">
+              <h2 class="display-1 font-weight-light">Community</h2>
+              <p class="subheading mt-4">
+                We find exchanging ideas with other researchers to be extremely inspiring. Discovery engine aims to bring the research community together by allowing people to comment on and discuss about existing works.
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+      <section class="py-5 my-5">
+        <v-container>
+          <v-layout row wrap align-center>
+            <v-flex xs12 md6>
+              <v-parallax height=350 class="collection"
+                :src="getImgUrl('graph-insitu.png')">
+              </v-parallax>
+            <!-- Photo by Hans-Peter Gauster on Unsplash -->
+            </v-flex>
+            <v-flex xs12 md6 class="py-5" :px-5="$vuetify.breakpoint.smAndUp"
+              :px-4="$vuetify.breakpoint.xsOnly">
+              <h2 class="display-1 font-weight-light">Reference Management</h2>
+              <p class="subheading mt-4">
+                With the most accurate paper database and graph visualization, discovery engine provides novel ways to collect and organize references. Manually editing bibliography files will finally become a history.
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+      <section id="sign-up" class="py-5 mt-5 grey darken-3 white--text">
+        <v-container grid-list-lg :px-5="$vuetify.breakpoint.smAndUp"
+          :px-4="$vuetify.breakpoint.xsOnly">
+          <h1 class="mt-2 pb-2 text--white">Sign up for product update:</h1>
           <v-form class="mt-2"
             :action="`https://formspree.io/${toEmail}`" method="POST">
             <v-layout wrap>
@@ -149,7 +142,7 @@ export default {
       props: {
         '--tx': {
           from: '-15%',
-          to: '15%'
+          to: '5%'
         }
       }
     })
@@ -163,7 +156,18 @@ export default {
   text-align: center;
 }
 
-@media screen and (min-width: 668px) {
+.container {
+  max-width: 1185px;
+  padding: 0px;
+}
+
+@media screen and (min-width: 1080px) {
+  .responsive-font-size {
+    font-size: 75px;
+  }
+}
+
+@media screen and (max-width: 1079px) {
   .responsive-font-size {
     font-size: 60px;
   }
@@ -171,7 +175,7 @@ export default {
 
 @media screen and (max-width: 667px) {
   .responsive-font-size {
-    font-size: 9vw;
+    font-size: 10vw;
   }
 }
 
