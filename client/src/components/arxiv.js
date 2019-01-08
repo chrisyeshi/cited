09f1e99dd71afe6fec66c45e054167c2c1e70fbd
Chris Ye <chrisyeshi@gmail.com>
2019-01-08 15:43:29 -0800

    try arxiv api

---
+++ client/src/components/arxiv.js
@@ -0,0 +1,41 @@
+import query from './arxivwrapper.js'
+
+export default {
+  async signIn (email, password) {
+    return null
+  },
+
+  async logout () {
+    return null
+  },
+
+  async me () {
+    return null
+  },
+
+  async searchRefObjs (text) {
+    const results = await query({
+      searchQuery: `all:${text}`,
+      start: 0,
+      maxResults: 20
+    })
+    console.log(results)
+    return []
+  },
+
+  async getRefObj (refObjId) {
+    return null
+  },
+
+  async getReferences (refObjId) {
+    return { refObj: null, references: [] }
+  },
+
+  async getCitedBys (refObjId) {
+    return { refObj: null, citedBys: [] }
+  },
+
+  async getCommonRelatives (refObjIds) {
+    return { refObjs: [], relatives: [] }
+  }
+}
---
+++ client/src/components/arxivwrapper.js
@@ -0,0 +1,40 @@
+import { parseString } from 'xml2js'
+import _ from 'lodash'
+
+const baseUrl = 'http://export.arxiv.org/api'
+const queryUrl = `${baseUrl}/query`
+
+function buildQueryParaStr (
+  { searchQuery, idList, start, maxResults, sortBy, sortOrder }) {
+  const searchQueryStr =
+    _.isNil(searchQuery) ? '' : `search_query=${searchQuery}`
+  const idListStr = _.isNil(idList) ? '' : `id_list=${_.join(idList, ',')}`
+  const startStr = _.isNil(start) ? '' : `start=${start}`
+  const maxResultsStr = _.isNil(maxResults) ? '' : `max_results=${maxResults}`
+  const sortByStr = _.isNil(sortBy) ? '' : `sortBy=${sortBy}`
+  const sortOrderStr = _.isNil(sortOrder) ? '' : `sortOrder=${sortOrder}`
+  const paraStrArray = [
+    searchQueryStr, idListStr, startStr, maxResultsStr, sortByStr, sortOrderStr
+  ]
+  return _.join(_.filter(paraStrArray, str => str.length > 0), '&')
+}
+
+export default async function query (
+  { searchQuery, idList, start, maxResults, sortBy }) {
+  const queryParaStr =
+    buildQueryParaStr({ searchQuery, idList, start, maxResults, sortBy })
+  console.log(queryParaStr)
+  const res = await fetch(`${queryUrl}?${queryParaStr}`)
+  if (!res.ok) {
+    throw new Error({ code: res.status, text: res.statusText })
+  }
+  const xml = await res.text()
+  return new Promise((resolve, reject) => {
+    parseString(xml, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(result)
+    })
+  })
+}
