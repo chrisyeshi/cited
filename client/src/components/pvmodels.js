efc75e706c3f09356c11b3de5b5ad5f72e99037d
Chris Ye <chrisyeshi@gmail.com>
2019-02-16 14:22:42 -0800

    chaotic implementation of the paths

--- client/src/components/pvmodels.js
+++ client/src/components/pvmodels.js
@@ -1,13 +1,9 @@
+import _ from 'lodash'
+
 export class Author {
   constructor (surname, given) {
     this.surname = surname
     this.given = given
-  }
-}
-
-export class Organization {
-  constructor (name) {
-    this.name = name
   }
 }
 
@@ -19,7 +15,63 @@ export class AffiliatedAuthor {
   get surname () { return this.author.surname }
 }
 
-export class Venue {
+export class Article {
+  constructor (type, data, nReferences, references, nCitedBys, citedBys) {
+    this.type = type
+    this.data = data
+    this.nReferences = nReferences
+    this.references = references
+    this.nCitedBys = nCitedBys
+    this.citedBys = citedBys
+  }
+}
+
+export class Graph {
+  constructor (nodes) {
+    this.nodes = nodes || []
+  }
+
+  getAllPathsBetween (reference, citedBy) {
+    return this.getAllPathsBetweenRecursive(reference, citedBy, [])
+  }
+
+  getAllPathsBetweenRecursive (reference, citedBy, path) {
+    if (reference === citedBy) {
+      return [ path ]
+    }
+    if (reference.inGraphCitedBys.length === 0) {
+      return false
+    }
+    const paths =
+      _.concat(
+        ..._.map(reference.inGraphCitedBys, currCitedBy => {
+          const paths = this.getAllPathsBetweenRecursive(
+            currCitedBy, citedBy,
+            [ ...path, new Link(reference, currCitedBy) ])
+          return paths
+        }))
+    return _.filter(paths)
+  }
+
+  get isEmpty () { return this.nodes.length === 0 }
+
+  get links () {
+    return _.flatten(_.map(this.nodes, (node, index) => {
+      const citedBys = node.inGraphCitedBys
+      return _.map(citedBys, citedBy => new Link(node, citedBy))
+    }))
+  }
+}
+
+export class Node {
+  constructor (article, inGraphReferences, inGraphCitedBys) {
+    this.article = article
+    this.inGraphReferences = inGraphReferences
+    this.inGraphCitedBys = inGraphCitedBys
+  }
+}
+
+export class Organization {
   constructor (name) {
     this.name = name
   }
@@ -35,29 +87,15 @@ export class Paper {
   }
 }
 
-export class Article {
-  constructor (type, data, nReferences, references, nCitedBys, citedBys) {
-    this.type = type
-    this.data = data
-    this.nReferences = nReferences
-    this.references = references
-    this.nCitedBys = nCitedBys
-    this.citedBys = citedBys
+export class Link {
+  constructor (reference, citedBy) {
+    this.reference = reference
+    this.citedBy = citedBy
   }
 }
 
-export class Node {
-  constructor (article, inGraphReferences, inGraphCitedBys) {
-    this.article = article
-    this.inGraphReferences = inGraphReferences
-    this.inGraphCitedBys = inGraphCitedBys
+export class Venue {
+  constructor (name) {
+    this.name = name
   }
-}
-
-export class Graph {
-  constructor (nodes) {
-    this.nodes = nodes || []
-  }
-
-  get isEmpty () { return this.nodes.length === 0 }
 }
