cc9ecac05bb1c9ab9d173823133d5e4d5bbb2b32
Chris Ye <chrisyeshi@gmail.com>
2019-05-21 12:12:51 -0700

    sign in with aws cognito and amplify

--- client/src/components/SignInButton.vue
+++ client/src/components/SignInButton.vue
@@ -1,71 +1,68 @@
 <template>
-  <v-layout fill-height>
-    <v-menu
-      v-show="!$store.getters.isSignedIn" offset-y
-      :close-on-content-click="false"
-      v-model="isSignInMenuVisible">
-      <v-btn slot="activator" flat key="text">SIGN IN</v-btn>
-      <v-card>
-        <v-card-title>
-          <v-form @submit.prevent="signIn">
-            <v-text-field
-              ref="email"
-              label="E-mail"
-              v-model="email"
-              required
-            ></v-text-field>
-            <v-text-field
-              ref="password"
-              label="Password"
-              type="password"
-              v-model="password"
-              id="id"
-            ></v-text-field>
-            <v-btn type="submit" color="primary">submit</v-btn>
-          </v-form>
-        </v-card-title>
-      </v-card>
-    </v-menu>
-    <v-menu v-show="$store.getters.isSignedIn" offset-y>
-      <v-btn slot="activator" large icon>
-        <v-icon large color="grey darken-2" key="icon">account_circle</v-icon>
-      </v-btn>
-      <v-list>
-        <v-subheader>{{ currEmail }}</v-subheader>
-        <v-list-tile @click="logout">
-          <v-list-tile-title>Logout</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-menu>
-  </v-layout>
+  <v-btn v-if="!isSignedIn" flat small @click="signIn">Sign In</v-btn>
+  <v-menu v-else offset-y>
+    <v-btn slot="activator" large icon>
+      <v-icon v-if="!userImg" large color="grey darken-2" key="icon">
+        account_circle
+      </v-icon>
+      <v-avatar size="36px">
+        <img :src="userImg">
+      </v-avatar>
+    </v-btn>
+    <v-list>
+      <v-subheader>{{ userEmail }}</v-subheader>
+      <v-list-tile @click="logout">
+        <v-list-tile-title>Logout</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
 </template>
 
 <script>
+import { AmplifyEventBus } from 'aws-amplify-vue'
+
 export default {
   name: 'SignInButton',
   data () {
+    this.populateAuthState()
     return {
-      isSignInMenuVisible: false,
-      email: 'test@test.com',
-      password: 'password'
+      user: null
     }
   },
   computed: {
-    currEmail () {
-      const currUser = this.$store.state.currUser
-      return currUser ? currUser.email : 'Unknown'
+    isSignedIn () {
+      return this.user
+    },
+    userEmail () {
+      return this.isSignedIn ? this.user.attributes.email : null
+    },
+    userImg () {
+      return this.isSignedIn ? this.user.attributes.picture : null
     }
   },
   methods: {
     signIn () {
-      this.isSignInMenuVisible = false
-      this.$store.dispatch(
-        'signIn',
-        { email: this.$refs.email.value, password: this.$refs.password.value })
+      this.$Amplify.Auth.federatedSignIn({ provider: 'Google' })
     },
     logout () {
-      this.$store.dispatch('logout')
+      this.$Amplify.Auth.signOut()
+    },
+    async populateAuthState () {
+      try {
+        this.user = await this.$Amplify.Auth.currentAuthenticatedUser()
+      } catch (error) {
+        if (error === 'not authenticated') {
+          this.user = null
+        } else {
+          console.log(error)
+        }
+      }
     }
+  },
+  created () {
+    AmplifyEventBus.$on('authState', info => {
+      console.log(info)
+    })
   }
 }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,21 @@ import VueLineClamp from 'vue-line-clamp'
 import App from './App'
 import router from './router'
 import store from './store'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import awsmobile from '../aws-exports'
 
+const oauth = {
+  domain: 'disco.auth.us-east-2.amazoncognito.com',
+  scope: ['email', 'profile', 'openid'],
+  redirectSignIn: 'http://localhost:8080/demo/',
+  redirectSignOut: 'http://localhost:8080/demo/',
+  responseType: 'code' // or token
+}
+Amplify.configure(awsmobile)
+AmplifyModules.Auth.configure({ oauth })
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 Vue.use(VueLineClamp, { importCss: true })
 Vue.use(VueAsyncComputed)
 
