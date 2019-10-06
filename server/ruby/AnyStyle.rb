10c85ced02e58606e598179878a656577f86724c
Chris Ye <chrisyeshi@gmail.com>
2019-10-06 15:39:53 -0700

    remove unnecessary directories

--- server/ruby/AnyStyle.rb
+++
@@ -1,5 +0,0 @@
-require 'anystyle'
-require 'json'
-
-output = AnyStyle.parse ARGV
-puts output.to_json
--- server/ruby/test.rb
+++
@@ -1,9 +0,0 @@
-require 'anystyle'
-require 'json'
-
-output = AnyStyle.parse [
-  'Derrida, J. (1967). L’écriture et la différence (1 éd.). Paris: Éditions du Seuil.',
-  'Poe, Edgar A. Essays and Reviews. New York: Library of America, 1984.'
-]
-
-puts output.to_json
